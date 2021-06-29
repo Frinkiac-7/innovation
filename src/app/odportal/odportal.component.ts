28c96f4e334856e8d7bd9ec080dcb1926031df3c
frinkiac-7.net <rob@frinkiac-7.net>
2021-06-29 14:54:06 -0400

    Replicate general UI and layout to all components   * Fixed icon size discrepancy in the home component   * Added two new images for use in hero divs on remaining pages   * Extended the Tailwind CSS theme config to use new image assets     - Classes are accessible as bg-opendata and bg-team   * Added icons to cards in the open data portal component Caveats:   * UI tweaks and improvements remain but this is sufficient for now   * Images are full sized and still need to be downloaded

--- src/app/odportal/odportal.component.ts
+++ src/app/odportal/odportal.component.ts
@@ -1,5 +1,6 @@
 import { RouterTestingModule } from '@angular/router/testing';
 import { Component, OnInit } from '@angular/core';
+import { faChartLine, faEye, faStar, faStarHalfAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-odportal',
@@ -7,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./odportal.component.css']
 })
 export class OdportalComponent implements OnInit {
+
+	faChartLine = faChartLine;
+	faEye = faEye;
+	faStar = faStar;
 
   constructor() { }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,9 @@ module.exports = {
   theme: {
     extend: {
 			backgroundImage: theme => ({
-				'pathway': "url('src/assets/innovation-office-pathways.jpg')"
+				'pathway': "url('src/assets/innovation-office-pathways.jpg')",
+				'opendata': "url('src/assets/jared-arango-1-mh6U3qeGQ-unsplash.jpg')",
+				'team': "url('src/assets/marvin-meyer-SYTO3xs06fU-unsplash.jpg')",
 				// 'footer-texture': "url('/img/footer-texture.png')",
 	 		})
 		},
