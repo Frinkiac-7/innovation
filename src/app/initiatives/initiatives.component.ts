db0d3e241344a5065ae53eb4ff269e9b332f62a5
frinkiac-7.net <rob@frinkiac-7.net>
2021-07-22 13:25:18 -0400

    Add content to Initiatves component   * Populate initiatives from city website   * Added icon to indicate external links

--- src/app/initiatives/initiatives.component.ts
+++ src/app/initiatives/initiatives.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-initiatives',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InitiativesComponent implements OnInit {
 
+	faExtLinkAlt = faExternalLinkAlt;
+	
   constructor() { }
 
   ngOnInit(): void {
