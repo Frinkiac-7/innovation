ab66eb7eecbf6d60b59df62e756d025870817d24
frinkiac-7.net <rob@frinkiac-7.net>
2021-07-26 15:11:16 -0400

    Extend CORS config   * Testing with herokuapp URL first

--- server.js
+++ server.js
@@ -10,10 +10,27 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+const whitelist = ['https://innosb.herokuapp.com']; // list of allow domain
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin) {
+            return callback(null, true);
+        }
+
+        if (whitelist.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}
+
+// end 
 app.use(requireHTTPS);
-app.use(cors({
-	origin: '*', 
-}));
+
+app.use(cors(corsOptions));
 
 app.use(express.static('./dist/innovation'));
 app.get('/*', function(req, res) {
@@ -22,3 +39,4 @@ app.get('/*', function(req, res) {
 });
 
 app.listen(process.env.PORT || 8080);
+
