edaea003f826bf8df79be5d6b000adb521c3d820
frinkiac-7.net <rob@frinkiac-7.net>
2021-10-05 09:20:46 -0400

    Add ODP Component to resolve build issue   * OPD component in previous build used API function which has changed   * Function definition in service now requires a param   * ODP component is breaking on build as it doesn't include the param

--- src/app/odportal/odportal.component.ts
+++ src/app/odportal/odportal.component.ts
@@ -1,8 +1,4 @@
-import { OdpapiService } from './../services/odpapi.service';
-import { RouterTestingModule } from '@angular/router/testing';
 import { Component, OnInit } from '@angular/core';
-
-import { Odp } from '../interfaces/odp';
 @Component({
   selector: 'app-odportal',
   templateUrl: './odportal.component.html',
@@ -10,48 +6,9 @@ import { Odp } from '../interfaces/odp';
 })
 export class OdportalComponent implements OnInit {
 
-	public totalDatasets!: Odp;
-	public totalRecords!: Odp;
-	public serviceResponse!: Odp;
-	public pkg!: Odp;
+  constructor() { }
 
-  getAPIData() {
-		this.api.getFullApiResponse().subscribe((data: any) => {
-			this.serviceResponse = data;
-			this.totalRecords = data.result.records.length
-			console.log('this.totalRecords:', this.totalRecords)
-			console.log('this.serviceResponse:', this.serviceResponse)
-		});
-  }
-
-	getAPIRecords() {
-		this.api.getRecords().subscribe((data: any) => {
-			this.serviceResponse = data;
-			console.log('this.serviceResponse:', this.serviceResponse)
-		});
-  }
-
-  constructor(public api: OdpapiService) {
-
-	}
-	
   ngOnInit(): void {
-
-		this.api.getPkgList().subscribe((data: any) => {
-			this.pkg = data
-			this.totalDatasets = data.length;
-			console.log('this.pkg:', this.pkg)
-			console.log('this.totalDatasets:', this.totalDatasets)
-		})
-
-		this.api.getPkgsWithResources().subscribe((data: any) => {
-		this.totalDatasets = data.length;
-		})
-
-		this.api.getFullApiResponse().subscribe((data: any) => {
-			this.totalRecords = data.result.total
-		})
-		
-	}
+  }
 
 }
